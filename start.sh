69b5af208ccb95b24e623cae78e23b5e7ab71c3c
Albert Zak <me@albertzak.com>
2016-03-11 00:58:36 +0100

    Set output id via env var

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 mkdir -p /var/run/dbus
-shairport-sync -a "$AIRPLAY_NAME" -- -d hw:0 -t hardware -c "$OUTPUT_NAME"
+shairport-sync -a "$AIRPLAY_NAME" -- -d hw:$OUTPUT_ID -c "$OUTPUT_NAME"
