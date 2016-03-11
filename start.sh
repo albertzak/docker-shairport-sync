06f3647bdbc2f35ccf6301ff9aab1f599d2c8a7a
Albert Zak <me@albertzak.com>
2016-03-11 01:21:02 +0100

    Fix avahi

--- start.sh
+++ start.sh
@@ -1,4 +1,12 @@
 #!/bin/sh
 
 mkdir -p /var/run/dbus
+
+# Set host-name and enable-dbus
+sed -i -e "s@#enable-dbus=yes@enable-dbus=no@" -e "s@#host-name=foo@host-name=$(echo $RESIN_DEVICE_UUID | cut -c1-7)@" /etc/avahi/avahi-daemon.conf
+
+# Restart service
+/etc/init.d/avahi-daemon restart
+
+
 shairport-sync -a "$AIRPLAY_NAME" -- -d hw:$OUTPUT_ID -c "$OUTPUT_NAME"
