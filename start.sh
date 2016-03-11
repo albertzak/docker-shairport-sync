9b6d6bd8efd2a0eed9391c9a43566564c81e792a
Albert Zak <me@albertzak.com>
2016-03-11 12:32:07 +0100

    Use raspberrypi3-debian as base image

--- start.sh
+++ start.sh
@@ -2,9 +2,6 @@
 
 set -e
 
-echo "Starting dbus-daemon"
-dbus-daemon --system --fork
-
 # Set host-name and enable-dbus
 sed -i -e "s@#enable-dbus=yes@enable-dbus=yes@" -e "s@#host-name=foo@host-name=$(echo $RESIN_DEVICE_UUID | cut -c1-7)@" /etc/avahi/avahi-daemon.conf
 
