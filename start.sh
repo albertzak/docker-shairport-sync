e745073d06ff5386d2d40da6d55d2a01a73aa11f
Albert Zak <me@albertzak.com>
2016-03-11 01:44:51 +0100

    Enable debug for avahi

--- start.sh
+++ start.sh
@@ -1,14 +1,16 @@
 #!/bin/sh
 
-mkdir -p /var/run/dbus
-
 # Set host-name and enable-dbus
 sed -i -e "s@#enable-dbus=yes@enable-dbus=no@" -e "s@#host-name=foo@host-name=$(echo $RESIN_DEVICE_UUID | cut -c1-7)@" /etc/avahi/avahi-daemon.conf
 
 # Restart service
+echo "Stopping avahi-daemon"
 systemctl stop avahi-daemon
-avahi-daemon --daemonize
+
+echo "Starting avahi-daemon"
+avahi-daemon --debug
 
 sleep 10s
 
+echo "Starting shairport-sync"
 shairport-sync -a "$AIRPLAY_NAME" -- -d hw:$OUTPUT_ID -c "$OUTPUT_NAME"
