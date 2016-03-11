3e73042fc2550f2b7c9f5a1cedd32e9e1f193f2a
Albert Zak <me@albertzak.com>
2016-03-11 02:07:34 +0100

    Add dbus-daemon

--- start.sh
+++ start.sh
@@ -1,5 +1,12 @@
 #!/bin/sh
 
+set -e
+
+mkdir /var/run/dbus
+
+echo "Starting dbus-daemon"
+dbus-daemon --system --fork
+
 # Set host-name and enable-dbus
 sed -i -e "s@#enable-dbus=yes@enable-dbus=yes@" -e "s@#host-name=foo@host-name=$(echo $RESIN_DEVICE_UUID | cut -c1-7)@" /etc/avahi/avahi-daemon.conf
 
