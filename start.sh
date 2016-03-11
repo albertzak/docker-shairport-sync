f47dfbc4a9c75557f5c7e4ec9d64ed6fd601e842
Albert Zak <me@albertzak.com>
2016-03-11 01:57:02 +0100

    Enable dbus

--- start.sh
+++ start.sh
@@ -1,16 +1,16 @@
 #!/bin/sh
 
 # Set host-name and enable-dbus
-sed -i -e "s@#enable-dbus=yes@enable-dbus=no@" -e "s@#host-name=foo@host-name=$(echo $RESIN_DEVICE_UUID | cut -c1-7)@" /etc/avahi/avahi-daemon.conf
+sed -i -e "s@#enable-dbus=yes@enable-dbus=yes@" -e "s@#host-name=foo@host-name=$(echo $RESIN_DEVICE_UUID | cut -c1-7)@" /etc/avahi/avahi-daemon.conf
 
 # Restart service
 echo "Stopping avahi-daemon"
 systemctl stop avahi-daemon
 
 echo "Starting avahi-daemon"
-avahi-daemon --debug
+avahi-daemon -D
 
-sleep 10s
+sleep 5s
 
 echo "Starting shairport-sync"
 shairport-sync -a "$AIRPLAY_NAME" -- -d hw:$OUTPUT_ID -c "$OUTPUT_NAME"
