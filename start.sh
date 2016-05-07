968d1aaac154b49828362f80283f36e32d21e501
Albert Zak <me@albertzak.com>
2016-05-07 22:41:57 +0200

    Fix dbus startup issues

--- start.sh
+++ start.sh
@@ -3,11 +3,8 @@
 set -e
 
 echo "--> Starting dbus-daemon"
+rm /var/run/dbus/pid || true
 /usr/bin/dbus-daemon --system
-
-sleep 3s
-
-echo "--> Systemd processes: $(ps aux | grep systemd | grep -v grep)"
 
 # Set host-name and enable-dbus
 sed -i -e "s@#enable-dbus=yes@enable-dbus=yes@" -e "s@#host-name=foo@host-name=$(echo $RESIN_DEVICE_UUID | cut -c1-7)@" /etc/avahi/avahi-daemon.conf
@@ -19,7 +16,7 @@ systemctl stop avahi-daemon
 echo "--> Starting avahi-daemon"
 avahi-daemon --no-drop-root --daemonize
 
-sleep 3s
+sleep 1s
 
 echo "--> Starting shairport-sync"
 shairport-sync -a "$AIRPLAY_NAME" -- -d hw:$OUTPUT_ID
